903b7b76fd38bfaf0345be4b8308fc72b39c5725
JoseMarin <jmarinmalagon@gmail.com>
2023-05-26 17:58:35 +0200

    new project

---
+++ js/funciones.js
@@ -0,0 +1,27 @@
+//Suma de dos variables
+let A = 10;
+let B = 11;
+
+// let resultado = A + B;
+// console.log(resultado);
+
+//Funciones ES5
+function suma(valorA, valorB) {
+    return valorA + valorB;
+}
+
+let resultadoSuma = suma(3,6);
+
+alert(resultadoSuma);
+// suma(1,4);
+// suma(5,13);
+// suma(123,6);
+// suma(A,B);
+
+function resta(valorA,valorB) {
+    operacion = valorA - valorB;
+    console.log(operacion);
+}
+resta(7,3);
+
+//Funciones ES6
---
+++ js/funcionesES6.js
@@ -0,0 +1,17 @@
+//funcion suma ES5 ----------------------------- 1997-2014
+// var
+function suma(valorA, valorB) {
+    return valorA + valorB;
+}
+
+console.log(suma(2,3));
+//JQuery  --> AJAX
+
+//funcion ES6 --------------------------------------- 2015
+// Axios() o fetch()
+// let const 
+const suma = (valorA,valorB) => {
+    return valorA + valorB;
+}
+
+console.log(suma(2,3));
